d65c5dc1bcf224045d77aa8db8d5359b968e164f
Nathaniel Higgins <nat@nath.is>
2012-12-27 03:23:59 +0000

    Add a dynamic object class.

---
+++ system/classes/dynamic.php
@@ -0,0 +1,28 @@
+<?php defined('SCAFFOLD') or die;
+
+/**
+ * A dynamic object that supports properties and methods.
+ */
+class Dynamic {
+
+	/**
+	 * Constructor function. An array of methods and functions
+	 */
+	public function __construct(array $arr) {
+		foreach ($arr as $key => $val) {
+			$this->$key = $val;
+		}
+	}
+
+	/**
+	 * Handle method calling
+	 */
+	public function __call($name, $args) {
+		if (property_exists($this, $name) && is_callable($this->$name)) {
+			call_user_func_array($this->$name, $args);
+		} else {
+			throw new Exception('Method ' . $name . ' not found');
+		}
+	}
+
+}

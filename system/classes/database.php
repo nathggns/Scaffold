ddfeabd95f2dec9dcde374d411af2a736c5ed830
Nathaniel Higgins <nat@nath.is>
2013-01-29 22:16:22 +0000

    Rethink the query builder.

--- system/classes/database.php
+++ system/classes/database.php
@@ -17,6 +17,82 @@ class Database {
         $this->driver = Service::get('database.driver', $config->get('database'));
     }
 
+    /* Complex Query Helpers */
+    public static function where_or($val) {
+        return static::query($val, static::query_arr('or'));
+    }
+
+    public static function where_and($val) {
+        return static::query($val, static::query_arr('and'));
+    }
+
+    public static function where_equals($val) {
+        return static::query($val, static::query_arr('equals'));
+    }
+
+    public static function where_gt($val) {
+        return static::query($val, static::query_arr('gt'));
+    }
+
+    public static function where_gte($val) {
+        return static::query($val, static::query_arr('gte'));
+    }
+
+    public static function where_lt($val) {
+        return static::query($val, static::query_arr('lt'));
+    }
+
+    public static function where_lte($val) {
+        return static::query($val, static::query_arr('lte'));
+    }
+
+    public static function where_not($val) {
+        return static::query($val, static::query_arr('not'));
+    }
+
+    public static function query_arr($name) {
+        switch ($name) {
+            case 'not':
+                $prop = 'special';
+                $name = ['not'];
+            break;
+
+            case 'or':
+            case 'and':
+                $prop = 'connector';
+            break;
+
+            case 'gt':
+            case 'gte':
+            case 'lt':
+            case 'lte':
+            case 'equals':
+                $prop = 'operator';
+            break;
+
+            default: return;
+        }
+
+        return [$prop => $name];
+    }
+
+    public static function query($val, $opts) {
+
+        $obj = new Dynamic([]);
+
+        if (is_object($val)) {
+            $opts = array_merge(get_object_vars($val), $opts);
+        } else {
+            $opts['val'] = $val;
+        }
+
+        foreach ($opts as $key => $val) {
+            $obj->$key = $val;
+        }
+
+        return $obj;
+    }
+
     /**
      * Act like the driver.
      */

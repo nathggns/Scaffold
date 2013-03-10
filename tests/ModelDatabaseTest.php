aa1304407905ebe41dfd5b5d75c560e1f687b2b8
Nathaniel Higgins <nat@nath.is>
2013-03-10 11:32:50 +0000

    Sort out da tests.

--- tests/ModelDatabaseTest.php
+++ tests/ModelDatabaseTest.php
@@ -12,31 +12,35 @@ class MDT_ModelUser extends ModelDatabase {
 
 class ModelDatabaseTest extends PHPUnit_Framework_TestCase {
 
-    public function setUp() {
-        $builder = new DatabaseQueryBuilderSqlite();
-        $driver = new MDT_DatabaseDriverPDOTestClass($builder, [
-            'dsn' => 'sqlite:test.db'
-        ]);
+    static $names = ['Nat', 'Joe', 'Andrew', 'Claudio', 'Doug', 'Will', 'Matt', 'Alex'];
+    public static $driver;
+
+    public static function setUpBeforeClass() {
+
+        if (!static::$driver) {
+            $builder = new DatabaseQueryBuilderSqlite();
+            static::$driver = new MDT_DatabaseDriverPDOTestClass($builder, [
+                'dsn' => 'sqlite:test.db'
+            ]);
+        }
+
+        $driver = static::$driver;
 
         $driver->manual_query('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);');
 
-        $names = ['Nat', 'Joe', 'Andrew', 'Claudio', 'Doug', 'Will', 'Matt', 'Alex'];
-
-        foreach ($names as $name) {
+        foreach (static::$names as $name) {
             $driver->insert('users', [
                 'name' => $name
             ]);
         }
-
-        $this->driver = $driver;
     }
 
-    public function tearDown() {
+    public static function tearDownAfterClass() {
         unlink(ROOT . 'test.db');
     }
 
     public function get() {
-        return new MDT_ModelUser(null, $this->driver);
+        return new MDT_ModelUser(null, static::$driver);
     }
 
     public function equals($arr, $obj = false) {
@@ -51,12 +55,41 @@ class ModelDatabaseTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFindUserWithIdFromConstruct() {
-        $user = new MDT_ModelUser(1, $this->driver);
+        $user = new MDT_ModelUser(1, static::$driver);
 
         $this->equals([
             'id' => '1',
-            'name' => 'Nat'
+            'name' => 'Nat',
         ], $user);
+
+        $this->assertEquals(1, count($user));
+    }
+
+    public function testFetchUserWithId() {
+        $user = $this->get()->fetch(['id' => 1]);
+
+        $this->equals([
+            'id' => '1',
+            'name' => 'Nat',
+        ], $user);
+
+        $this->assertEquals(1, count($user));
+    }
+
+    public function testFetchAllUsers() {
+        $users = $this->get();
+
+        $this->assertEquals(8, count($users));
+        $this->assertEquals(8, $users->count());
+
+        foreach (static::$names as $i => $name) {
+            $user = $users[$i];
+
+            $this->equals([
+                'id' => (string) $i + 1,
+                'name' => $name
+            ], $user);
+        }
     }
 
 }

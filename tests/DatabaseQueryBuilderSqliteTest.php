b395969fd25af904934ecf21fd585c82cf74b30f
Nathaniel Higgins <nat@nath.is>
2013-06-01 20:10:59 +0100

    Add a failing sqlite test.

---
+++ tests/DatabaseQueryBuilderSqliteTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class DatabaseQueryBuilderSqliteTest extends DatabaseQueryBuilderSQLTest {
+
+    public function setUp() {
+        $this->builder = new DatabaseQueryBuilderSqlite();
+    }
+
+    public function testClear() {
+        $sql = $this->builder->clear('users');
+
+        $this->assertEquals('DELETE FROM `users`;', $sql);
+    }
+
+}
